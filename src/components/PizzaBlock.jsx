a579de26ebfdd0a52f01e950a9bd92199a5fcc91
gepiko <aoriaira@gmail.com>
2022-05-21 17:49:40 +0300

    added onClickAdd to button

--- src/components/PizzaBlock.jsx
+++ src/components/PizzaBlock.jsx
@@ -1,4 +1,12 @@
+import React from 'react'
+
 function PizzaBlock({ title, price }) {
+  const [pizzaCount, setPizzaCount] = React.useState(0)
+
+  const onClickAdd = () => {
+    setPizzaCount(pizzaCount + 1)
+  }
+
   return (
     <div className='pizza-block-wrapper'>
       <div className='pizza-block'>
@@ -23,7 +31,7 @@ function PizzaBlock({ title, price }) {
         </div>
         <div className='pizza-block__bottom'>
           <div className='pizza-block__price'>от {price} ₽</div>
-          <button className='button button--outline button--add'>
+          <button onClick={onClickAdd} className='button button--outline button--add'>
             <svg
               width='12'
               height='12'
@@ -37,6 +45,7 @@ function PizzaBlock({ title, price }) {
               ></path>
             </svg>
             <span>Добавить</span>
+            <i>{pizzaCount}</i>
           </button>
         </div>
       </div>
