95ee1147e2d91fd88670dc7e05a6c30adfe1d791
gepiko <aoriaira@gmail.com>
2022-05-22 17:53:03 +0300

    mapping the categories

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,13 +1,26 @@
+import React from 'react'
+
 function Categories() {
+  const [activeIndex, setActiveIndex] = React.useState(0)
+
+  const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые']
+
+  const onClickCategory = idx => {
+    setActiveIndex(idx)
+  }
+
   return (
     <div className='categories'>
       <ul>
-        <li className='active'>Все</li>
-        <li className=''>Мясные</li>
-        <li className=''>Вегетарианская</li>
-        <li className=''>Гриль</li>
-        <li className=''>Острые</li>
-        <li className=''>Закрытые</li>
+        {categories.map((category, i) => (
+          <li
+            key={i}
+            onClick={() => onClickCategory(i)}
+            className={activeIndex === i ? 'active' : ''}
+          >
+            {category}
+          </li>
+        ))}
       </ul>
     </div>
   )
