4e47579a468c6003fc534c8f085518dff8dad43c
gepiko <aoriaira@gmail.com>
2022-06-12 18:28:33 +0300

    quick fix to add item to cart

--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -25,11 +25,10 @@ const cartSlice = createSlice({
           ...action.payload,
           count: 1,
         })
-
-        state.totalPrice = state.items.reduce((sum, obj) => {
-          return obj.price + sum
-        }, 0)
       }
+      state.totalPrice = state.items.reduce((sum, obj) => {
+        return obj.price * obj.count + sum
+      }, 0)
     },
     removeItem(state, action) {
       state.items = state.items.filter(obj => obj.id !== action.payload)
