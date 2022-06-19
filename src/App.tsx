1d01ea760f2953b9e688f31f405cefade33367ef
gepiko <aoriaira@gmail.com>
2022-06-19 15:43:35 +0300

    added React.lazy for Cart, fullPizza and notFound

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,44 @@
+import React, { Suspense } from 'react'
 import { Routes, Route } from 'react-router-dom'
 
 import Home from './pages/Home'
-import Cart from './pages/Cart'
-import FullPizza from './pages/FullPizza'
-import NotFound from './pages/NotFound'
-import MainLayout from './layouts/MainLayout'
 
+import MainLayout from './layouts/MainLayout'
 import './scss/app.scss'
+
+const Cart = React.lazy(() => import('./pages/Cart'))
+const FullPizza = React.lazy(() => import('./pages/FullPizza'))
+const NotFound = React.lazy(() => import('./pages/NotFound'))
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<MainLayout />}>
         <Route path='/' element={<Home />} />
-        <Route path='/cart' element={<Cart />} />
-        <Route path='/pizza/:id' element={<FullPizza />} />
-        <Route path='*' element={<NotFound />} />
+        <Route
+          path='/cart'
+          element={
+            <Suspense fallback='Загрузка корзины...'>
+              <Cart />
+            </Suspense>
+          }
+        />
+        <Route
+          path='/pizza/:id'
+          element={
+            <Suspense fallback='Загрузка...'>
+              <FullPizza />
+            </Suspense>
+          }
+        />
+        <Route
+          path='*'
+          element={
+            <Suspense fallback='Загрузка...'>
+              <NotFound />
+            </Suspense>
+          }
+        />
       </Route>
     </Routes>
   )
