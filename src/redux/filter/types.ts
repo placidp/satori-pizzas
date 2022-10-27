a6f0a16e342391ddf0d6a5724319c36532e4c359
gepiko <aoriaira@gmail.com>
2022-10-27 15:39:27 +0300

    fix sort by title

--- src/redux/filter/types.ts
+++ src/redux/filter/types.ts
@@ -1,8 +1,8 @@
 export enum SortPropertyEnum {
   RATING_DESC = 'rating',
   RATING_ASC = '-rating',
-  TITLE_DESC = 'title',
-  TITLE_ASC = '-title',
+  TITLE_DESC = 'name',
+  TITLE_ASC = '-name',
   PRICE_DESC = 'price',
   PRICE_ASC = '-price',
 }
