63429763251d3446236c6e14ddd9d9ebd49e0095
gepiko <aoriaira@gmail.com>
2022-11-11 11:00:42 +0300

    WIP - FullPizzapage - fix styles & logic bugs

--- src/pages/FullPizza.tsx
+++ src/pages/FullPizza.tsx
@@ -4,15 +4,18 @@ import { useParams, useNavigate } from 'react-router-dom'
 import { useGetItemQuery } from '../redux/itemsApi'
 
 import { selectCartItemByIdTypeSize } from '../redux/cart/selectors'
-import { useAppSelector } from '../redux/store'
+import { useAppDispatch, useAppSelector } from '../redux/store'
 import { PizzaSizes } from '../redux/pizza/types'
+import { addItem } from '../redux/cart/slice'
+import { CartItem } from '../redux/cart/types'
 
 const pizzaTypes = ['тонкое', 'традиционное']
 
 const FullPizza: FC = () => {
-  const params = useParams<{ id?: string }>()
+  const { id } = useParams()
   const navigate = useNavigate()
-  const { data: pizza, isLoading, isError } = useGetItemQuery(params.id ? params.id : '1')
+  const dispatch = useAppDispatch()
+  const { data: pizza, isLoading, isError } = useGetItemQuery(id || '1')
 
   const [activeType, setActiveType] = useState(0)
   const [activeSize, setActiveSize] = useState(0)
@@ -21,7 +24,7 @@ const FullPizza: FC = () => {
   const types = pizza?.types ? pizza.types : [0, 1]
 
   const hasItemInCart = useAppSelector(
-    selectCartItemByIdTypeSize(params.id!, pizzaTypes[activeType], sizes[activeSize])
+    selectCartItemByIdTypeSize(id!, pizzaTypes[activeType], sizes[activeSize])
   )
 
   if (isLoading) {
@@ -40,7 +43,18 @@ const FullPizza: FC = () => {
 
   const addedCount = hasItemInCart?.count
 
-  const onClickAdd = () => {}
+  const onClickAdd = () => {
+    const item: CartItem = {
+      id: pizza.id,
+      name: pizza.name,
+      price,
+      imageUrl: pizza.imageUrl,
+      type: pizzaTypes[activeType],
+      size: sizes[activeSize],
+      count: 0,
+    }
+    dispatch(addItem(item))
+  }
 
   // pizza types change price
   let price: number = pizza.prices.small!
@@ -53,18 +67,12 @@ const FullPizza: FC = () => {
     price = pizza.prices.small
   }
 
-  if (!pizza) {
-    return (
-      <>
-        {/* Загрузка... */}
-        {/* <Skeleton /> */}
-      </>
-    )
-  }
-
   return (
     <div className='fullPizzaContainer'>
-      <div className='pizza-block' style={{ width: '45%' }}>
+      <div
+        className='pizza-block'
+        // style={{ width: '45%' }}
+      >
         <a key={1}>
           <img
             className='pizza-block__image'
@@ -77,63 +85,72 @@ const FullPizza: FC = () => {
       <div className='descriptionContainer'>
         <div className='composition'>
           <h4 className='pizza-block__title'>{pizza.name}</h4>
-          <p>Моцарелла, сыры чеддер и пармезан, фирменный соус альфредо</p>
+          <p>
+            {/* Моцарелла, сыры чеддер и пармезан, фирменный соус альфредо */}
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga, quidem!
+          </p>
+          <br />
           Состав:
           <p>
-            Тесто дрожжевое, сыр моцарелла, соус томатный, сушеный базилик. <br />
+            {/* Тесто дрожжевое, сыр моцарелла, соус томатный, сушеный базилик. <br />
             Пищевая ценность на 100 г.: Белки: 13.7 г. Жиры: 11.9 г. Углеводы: 38.4 г. <br />
-            Энергетическая ценность: 315.9 ккал / 1320.7 кДж
+            Энергетическая ценность: 315.9 ккал / 1320.7 кДж */}
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde praesentium ipsa
+            repellendus saepe soluta distinctio commodi minus, officiis quasi quo, assumenda ratione
+            sint. Repudiandae, nostrum.
           </p>
         </div>
-        <div
-          className='pizza-block__selector'
-          style={{ width: '100%', alignItems: 'center', textAlign: 'center', padding: '8px' }}
-        >
-          <ul style={{ width: '100%' }}>
-            {types.map(type => (
-              <li
-                key={type}
-                onClick={() => setActiveType(type)}
-                className={activeType === type ? 'active' : ''}
-              >
-                {pizzaTypes[type]}
-              </li>
-            ))}
-          </ul>
-          <ul style={{ width: '100%' }}>
-            {sizes.map((size, i) => (
-              <li
-                key={size}
-                onClick={() => setActiveSize(i)}
-                className={activeSize === i ? 'active' : ''}
-              >
-                {size} см.
-              </li>
-            ))}
-          </ul>
-        </div>
         <div className='pizza-block__bottom' style={{ width: '100%' }}>
-          <div className='pizza-block__price'>от {price} ₽</div>
-          <button
-            onClick={onClickAdd}
-            className='button button--outline button--add'
-            style={{ width: '50%' }}
+          <div
+            className='pizza-block__selector'
+            style={{ width: '100%', alignItems: 'center', textAlign: 'center', padding: '8px' }}
           >
-            <svg
-              width='12'
-              height='12'
-              viewBox='0 0 12 12'
-              fill='none'
-              xmlns='http://www.w3.org/2000/svg'
+            <ul style={{ width: '100%' }}>
+              {types.map(type => (
+                <li
+                  key={type}
+                  onClick={() => setActiveType(type)}
+                  className={activeType === type ? 'active' : ''}
+                >
+                  {pizzaTypes[type]}
+                </li>
+              ))}
+            </ul>
+            <ul style={{ width: '100%' }}>
+              {sizes.map((size, i) => (
+                <li
+                  key={size}
+                  onClick={() => setActiveSize(i)}
+                  className={activeSize === i ? 'active' : ''}
+                >
+                  {size} см.
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className='pizza-block__actions'>
+            <button
+              onClick={onClickAdd}
+              className='button button--outline button--add'
+              style={{ width: '50%' }}
             >
-              <path
-                d='M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z'
-                fill='white'
-              ></path>
-            </svg>
-            <span>Добавить</span>
-            {addedCount && <i>{addedCount}</i>}
-          </button>
+              <svg
+                width='12'
+                height='12'
+                viewBox='0 0 12 12'
+                fill='none'
+                xmlns='http://www.w3.org/2000/svg'
+              >
+                <path
+                  d='M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z'
+                  fill='white'
+                ></path>
+              </svg>
+              <span>Добавить</span>
+              {addedCount && <i>{addedCount}</i>}
+            </button>
+            <span className='pizza-block__price'>от {price} ₽</span>
+          </div>
         </div>
       </div>
     </div>
