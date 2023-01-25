c7beb9621d24a828b6ed609d53f0ac921891f3a9
gepiko <aoriaira@gmail.com>
2023-01-25 20:40:23 +0300

    fix: deleted unused selector

--- src/redux/cart/selectors.ts
+++ src/redux/cart/selectors.ts
@@ -2,9 +2,6 @@ import { RootState } from '../store'
 
 export const selectCart = (state: RootState) => state.cart
 
-export const selectCartItemById = (id: string) => (state: RootState) =>
-  state.cart.items.find(obj => obj.id === id)
-
 export const selectCartItemByIdTypeSize =
   (id: string, type: string, size: number) => (state: RootState) =>
     state.cart.items.find(obj => obj.id === id && obj.type === type && obj.size === size)
