6005fdc9ea139c45d7aee485c14853a64df87459
gepiko <aoriaira@gmail.com>
2023-03-13 19:40:27 +0300

    test cartSlice totalPrice

--- src/redux/cart/slice.test.ts
+++ src/redux/cart/slice.test.ts
@@ -1,10 +1,31 @@
-import cart, { addItem, clearItems, minusItem } from './slice'
+import cart, { addItem, clearItems, minusItem, removeItem } from './slice'
 import { getCartFromLS } from '../../utils/getCartFromLS'
 import { CartItem, CartSliceState } from './types'
+import { configureStore } from '@reduxjs/toolkit'
 
 const initialState: CartSliceState = getCartFromLS()
 
 describe('cartSlice', () => {
+  const cartItems = [
+    {
+      id: '1',
+      name: 'Пепперони',
+      price: 100,
+      imageUrl: 'urlToPizza1',
+      type: '1',
+      size: 1,
+      count: 1,
+    },
+    {
+      id: '2',
+      name: 'Маргарита',
+      price: 200,
+      imageUrl: 'urlToPizza2',
+      type: '2',
+      size: 2,
+      count: 2,
+    },
+  ]
   const cartItem: CartItem = {
     id: '1',
     name: 'Пепперони',
@@ -14,6 +35,14 @@ describe('cartSlice', () => {
     size: 1,
     count: 1,
   }
+
+  const store = configureStore({
+    reducer: cart,
+    preloadedState: {
+      items: cartItems,
+      totalPrice: 500,
+    },
+  })
 
   const state = {
     items: [{ ...cartItem, count: 2 }],
@@ -58,5 +87,40 @@ describe('cartSlice', () => {
 
     expect(result.items).toEqual([])
     expect(result.totalPrice).toBe(0)
+  })
+
+  it('should correctly update the totalPrice when an item is added', () => {
+    const newItem = {
+      id: '3',
+      name: 'Крейзи пепперони',
+      price: 300,
+      imageUrl: 'urlToPizza3',
+      type: '3',
+      size: 3,
+      count: 1,
+    }
+
+    store.dispatch(addItem(newItem))
+
+    const expectedTotalPrice = 500 + newItem.price
+    expect(store.getState().totalPrice).toBe(expectedTotalPrice)
+  })
+
+  it('should correctly update the totalPrice when an item is removed', () => {
+    const itemToRemove = {
+      id: '3',
+      name: 'Крейзи пепперони',
+      price: 300,
+      imageUrl: 'urlToPizza3',
+      type: '3',
+      size: 3,
+      count: 1,
+    }
+
+    store.dispatch(removeItem(itemToRemove))
+
+    const expectedTotalPrice = 500
+
+    expect(store.getState().totalPrice).toEqual(expectedTotalPrice)
   })
 })
